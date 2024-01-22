5a7bfd941ec04068b7a770859080b69e46ee14e2
belloLucas <lucasbellodev@gmail.com>
2024-01-22 17:26:32 -0300

    feat: Logic to open and close box color created

--- src/components/boxTask/BoxTask.tsx
+++ src/components/boxTask/BoxTask.tsx
@@ -1,10 +1,24 @@
-import { FaRegStar, FaStar } from "react-icons/fa";
+import { FaRegStar } from "react-icons/fa";
+// import {  FaStar } from "react-icons/fa";
 import { IoMdClose, IoMdColorFill } from "react-icons/io";
 import { GoPencil } from "react-icons/go";
+import BoxColor from "../boxColor/BoxColor";
 
 import "./BoxTask.scss";
+import { useState } from "react";
 
 export default function TaskBox() {
+  const [isColorBoxOpen, setIsColorBoxOpen] = useState(false);
+
+  const handleColorBoxOpen = () => {
+    setIsColorBoxOpen(true);
+  };
+
+  const handleColorSelect = (selectedColor: string) => {
+    console.log(selectedColor);
+    setIsColorBoxOpen(false);
+  };
+
   return (
     <div className="boxTask">
       <div className="header">
@@ -21,16 +35,22 @@ export default function TaskBox() {
       <div className="buttons">
         <div className="left">
           <span className="edit">
-            <GoPencil></GoPencil>
+            <GoPencil />
           </span>
-          <span className="paint">
-            <IoMdColorFill></IoMdColorFill>
+          <span className="paint" onClick={handleColorBoxOpen}>
+            <IoMdColorFill />
           </span>
         </div>
         <span className="remove">
-          <IoMdClose></IoMdClose>
+          <IoMdClose />
         </span>
       </div>
+      {isColorBoxOpen && (
+        <BoxColor
+          onSelect={handleColorSelect}
+          onClose={() => setIsColorBoxOpen(false)}
+        />
+      )}
     </div>
   );
 }
