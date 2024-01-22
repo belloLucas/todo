c357b995f6dc1f4f480932ec250b3bf7d1cf522f
belloLucas <lucasbellodev@gmail.com>
2024-01-22 16:00:21 -0300

    feat: Creating a box to set the color of task box

---
+++ src/components/boxColor/BoxColor.tsx
@@ -0,0 +1,22 @@
+import "./BoxColor.scss";
+
+export default function BoxColor() {
+  return (
+    <div className="container">
+      <ul className="colors">
+        <li className="color light-blue"></li>
+        <li className="color light-green"></li>
+        <li className="color yellow"></li>
+        <li className="color light-red"></li>
+        <li className="color red"></li>
+        <li className="color blue"></li>
+        <li className="color purple"></li>
+        <li className="color lime"></li>
+        <li className="color orange"></li>
+        <li className="color light-grey"></li>
+        <li className="color grey"></li>
+        <li className="color brown"></li>
+      </ul>
+    </div>
+  );
+}
