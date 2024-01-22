08bdf6cd3441c6fe6af7dcf4c9165ba17eec13d0
belloLucas <lucasbellodev@gmail.com>
2024-01-22 15:05:58 -0300

    feat: 'others' row added

--- src/components/tasks/Tasks.tsx
+++ src/components/tasks/Tasks.tsx
@@ -6,88 +6,121 @@ import "./Tasks.scss";
 
 export default function Tasks() {
   return (
-    <section className="favorite">
-      <p className="title">Favoritas</p>
-      <div className="fav-row">
-        <div className="boxTask">
-          <div className="header">
-            <input type="text" name="title" id="title" placeholder="Título" />
-            <FaRegStar className="addToFavorite" />
-          </div>
-          <div className="content">
-            <textarea
-              name="description"
-              className="description"
-              placeholder="Criar nota..."
-            />
-          </div>
-          <div className="buttons">
-            <div className="left">
-              <span className="edit">
-                <GoPencil></GoPencil>
-              </span>
-              <span className="paint">
-                <IoMdColorFill></IoMdColorFill>
+    <>
+      <section className="favorite">
+        <p className="title">Favoritas</p>
+        <div className="fav-row">
+          <div className="boxTask">
+            <div className="header">
+              <input type="text" name="title" id="title" placeholder="Título" />
+              <FaRegStar className="addToFavorite" />
+            </div>
+            <div className="content">
+              <textarea
+                name="description"
+                className="description"
+                placeholder="Criar nota..."
+              />
+            </div>
+            <div className="buttons">
+              <div className="left">
+                <span className="edit">
+                  <GoPencil></GoPencil>
+                </span>
+                <span className="paint">
+                  <IoMdColorFill></IoMdColorFill>
+                </span>
+              </div>
+              <span className="remove">
+                <IoMdClose></IoMdClose>
               </span>
             </div>
-            <span className="remove">
-              <IoMdClose></IoMdClose>
-            </span>
           </div>
-        </div>
-        <div className="boxTask">
-          <div className="header">
-            <input type="text" name="title" id="title" placeholder="Título" />
-            <FaRegStar className="addToFavorite" />
-          </div>
-          <div className="content">
-            <textarea
-              name="description"
-              className="description"
-              placeholder="Criar nota..."
-            />
-          </div>
-          <div className="buttons">
-            <div className="left">
-              <span className="edit">
-                <GoPencil></GoPencil>
-              </span>
-              <span className="paint">
-                <IoMdColorFill></IoMdColorFill>
+          <div className="boxTask">
+            <div className="header">
+              <input type="text" name="title" id="title" placeholder="Título" />
+              <FaRegStar className="addToFavorite" />
+            </div>
+            <div className="content">
+              <textarea
+                name="description"
+                className="description"
+                placeholder="Criar nota..."
+              />
+            </div>
+            <div className="buttons">
+              <div className="left">
+                <span className="edit">
+                  <GoPencil></GoPencil>
+                </span>
+                <span className="paint">
+                  <IoMdColorFill></IoMdColorFill>
+                </span>
+              </div>
+              <span className="remove">
+                <IoMdClose></IoMdClose>
               </span>
             </div>
-            <span className="remove">
-              <IoMdClose></IoMdClose>
-            </span>
           </div>
-        </div>
-        <div className="boxTask">
-          <div className="header">
-            <input type="text" name="title" id="title" placeholder="Título" />
-            <FaRegStar className="addToFavorite" />
-          </div>
-          <div className="content">
-            <textarea
-              name="description"
-              className="description"
-              placeholder="Criar nota..."
-            />
-          </div>
-          <div className="buttons">
-            <div className="left">
-              <span className="edit">
-                <GoPencil></GoPencil>
-              </span>
-              <span className="paint">
-                <IoMdColorFill></IoMdColorFill>
+          <div className="boxTask">
+            <div className="header">
+              <input type="text" name="title" id="title" placeholder="Título" />
+              <FaRegStar className="addToFavorite" />
+            </div>
+            <div className="content">
+              <textarea
+                name="description"
+                className="description"
+                placeholder="Criar nota..."
+              />
+            </div>
+            <div className="buttons">
+              <div className="left">
+                <span className="edit">
+                  <GoPencil></GoPencil>
+                </span>
+                <span className="paint">
+                  <IoMdColorFill></IoMdColorFill>
+                </span>
+              </div>
+              <span className="remove">
+                <IoMdClose></IoMdClose>
               </span>
             </div>
-            <span className="remove">
-              <IoMdClose></IoMdClose>
-            </span>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className="others">
+        <p className="title">Outras</p>
+        <div className="others-row">
+          <div className="boxTask">
+            <div className="header">
+              <input type="text" name="title" id="title" placeholder="Título" />
+              <FaRegStar className="addToFavorite" />
+            </div>
+            <div className="content">
+              <textarea
+                name="description"
+                className="description"
+                placeholder="Criar nota..."
+              />
+            </div>
+            <div className="buttons">
+              <div className="left">
+                <span className="edit">
+                  <GoPencil></GoPencil>
+                </span>
+                <span className="paint">
+                  <IoMdColorFill></IoMdColorFill>
+                </span>
+              </div>
+              <span className="remove">
+                <IoMdClose></IoMdClose>
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
